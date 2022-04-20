5e5a595d2c1c7c47f0142c19fd8eb7f83663711b
Whitney Omoruyi <womoruyi@wustl.edu>
2022-04-20 14:05:01 -0500

    added arraylist messages and java imports

--- src/chatbot/ConditionalState.java
+++ src/chatbot/ConditionalState.java
@@ -1,8 +1,21 @@
 package chatbot;
 
 
-// fetch random conditional state messages
-public String fetchMessage() {
-    Random generator = new Random();
-    return messages.get(generator.nextInt(messages.size()));
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ConditionalState{
+	
+    private String id = "";
+    private ArrayList<String> messages;
+    private ArrayList keywords;
+    public String argument = "";
+    
+	// fetch random conditional state messages
+	public String fetchMessage() {
+	    Random generator = new Random();
+	    return messages.get(generator.nextInt(messages.size()));
+	}
+	
 }
+
