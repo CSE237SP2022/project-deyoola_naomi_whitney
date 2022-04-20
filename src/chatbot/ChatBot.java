e5f8b428d24575884f22801dbdab5c51bc5c1694
Whitney Omoruyi <womoruyi@wustl.edu>
2022-04-20 13:47:03 -0500

    Created another file called info analyzer which is data parsing

--- src/chatbot/ChatBot.java
+++ src/chatbot/ChatBot.java
@@ -1,12 +1,1 @@
 package chatbot;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
