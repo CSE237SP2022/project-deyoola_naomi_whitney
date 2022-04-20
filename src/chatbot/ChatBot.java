a5d85eaa8801e34447878aca8ca7d5120bb48925
Whitney Omoruyi <womoruyi@wustl.edu>
2022-04-20 13:56:25 -0500

    Implementing chat default states and regex dictionaries

--- src/chatbot/ChatBot.java
+++ src/chatbot/ChatBot.java
@@ -1,1 +1,27 @@
 package chatbot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChatBot{
+	//Holds total regex matches
+	private HashMap<String,String> regexDictionary;
+	
+	//Default state to start Bear Necessities ChatBot
+	String beginningCondition = "0";
+	
+	InformationAnalyzation analyzer;
+	
+	public Bot(String beginningCondition, InformationAnalyzation analyzer) {
+		regexDictionary = new HashMap<String,String>();
+        this.beginningCondition = beginningCondition;
+        this.analyzer = analyzer;
+	}
+	 // Fetch present state message
+    public String fetchMessage() {
+        CondidtionalState state = analyzer.getState(beginningCondition);
+        return replaceMatches(state.fetchMessage()).trim();
+    }
+	
+}
